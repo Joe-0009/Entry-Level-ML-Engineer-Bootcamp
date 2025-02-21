60fb7a0dc900ef096787e1824d9e9657066b7981
Joe-0009 <youssefrachidi1994@gmail.com>
2025-02-21 12:48:55 +0100

    ex03 almost done

---
+++ Week00/ex03/count.py
@@ -0,0 +1,25 @@
+import sys
+import string
+
+
+def text_analyzer(text=None):
+    if text is None:
+        text = input("What is the text to analyze?\n")
+    if not isinstance(text, str):
+        print("AssertionError: argument is not a string")
+        return 
+    printable = len(text)
+    upper = sum(1 for c in text if c.isupper())
+    lower = sum(1 for c in text if c.islower())
+    space = text.count(" ")
+    punctuation = sum(1 for c in text if c in string.punctuation)
+    print(f"The text contains {printable} character(s):")
+    print(f"- {upper} upper letter(s)")
+    print(f"- {lower} lower letter(s)")
+    print(f"- {punctuation} punctuation mark(s)")
+    print(f"- {space} space (s)")
+
+
+if __name__ == "__main__":
+    text_analyzer()
+        
