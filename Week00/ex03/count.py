0a5ad9793bca750d33249b55510cace269f8152b
Joe-0009 <youssefrachidi1994@gmail.com>
2025-02-21 12:55:35 +0100

    ex03 done)

--- Week00/ex03/count.py
+++ Week00/ex03/count.py
@@ -3,11 +3,15 @@ import string
 
 
 def text_analyzer(text=None):
+    """
+    This function counts the number of upper characters, lower characters,
+    punctuation and spaces in a given text.
+    """
     if text is None:
         text = input("What is the text to analyze?\n")
     if not isinstance(text, str):
         print("AssertionError: argument is not a string")
-        return 
+        return
     printable = len(text)
     upper = sum(1 for c in text if c.isupper())
     lower = sum(1 for c in text if c.islower())
@@ -22,4 +26,3 @@ def text_analyzer(text=None):
 
 if __name__ == "__main__":
     text_analyzer()
-        
